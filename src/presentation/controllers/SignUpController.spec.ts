ae7e230cd9313a788202fcd4381eb7dbc413b9b4
gbelther <belthergabriel@gmail.com>
2022-06-13 23:02:02 -0300

    test: ensure throw badRequest when name is not provided

---
+++ src/presentation/controllers/SignUpController.spec.ts
@@ -0,0 +1,30 @@
+import { faker } from '@faker-js/faker';
+import { MissingParamError } from '../errors';
+import { SignUpController } from './SignUpController';
+
+type MakeSutReturn = {
+  sut: SignUpController;
+};
+
+const makeSut = (): MakeSutReturn => {
+  const sut = new SignUpController();
+  return {
+    sut,
+  };
+};
+
+describe('SignUpController', () => {
+  it('should return 400 if no name is provided', async () => {
+    const { sut } = makeSut();
+    const httpRequest = {
+      body: {
+        email: faker.internet.email(),
+        password: faker.internet.password(),
+        passwordConfirmation: faker.internet.password(),
+      },
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError('name'));
+  });
+});
