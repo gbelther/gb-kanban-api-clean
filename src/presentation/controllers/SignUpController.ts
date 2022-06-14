ab224ac8053a7684b2f8e7a4215cef2433432b7e
gbelther <belthergabriel@gmail.com>
2022-06-13 23:21:14 -0300

    feat: ensure return throw if email is not valid

--- src/presentation/controllers/SignUpController.ts
+++ src/presentation/controllers/SignUpController.ts
@@ -1,8 +1,11 @@
 import { InvalidParamError, MissingParamError } from '../errors';
 import { badRequest, serverError, success } from '../helpers/httpHelper';
 import { Controller, HttpRequest, HttpResponse } from '../protocols';
+import { EmailValidator } from '../protocols/EmailValidator';
 
 export class SignUpController implements Controller {
+  constructor(private readonly emailValidator: EmailValidator) {}
+
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const requiredFields = [
@@ -21,6 +24,11 @@ export class SignUpController implements Controller {
 
       if (password !== passwordConfirmation) {
         return badRequest(new InvalidParamError('passwordConfirmation'));
+      }
+
+      const emailIsValid = this.emailValidator.isValid(email);
+      if (!emailIsValid) {
+        return badRequest(new InvalidParamError('email'));
       }
 
       return success({ name, email, password, passwordConfirmation });
