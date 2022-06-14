6e4a38b2d2f9b9d53a84a4de15db51be1af80623
gbelther <belthergabriel@gmail.com>
2022-06-13 23:03:25 -0300

    test: ensure throw badRequest when email is not provided

--- src/presentation/controllers/SignUpController.spec.ts
+++ src/presentation/controllers/SignUpController.spec.ts
@@ -27,4 +27,18 @@ describe('SignUpController', () => {
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError('name'));
   });
+
+  it('should return 400 if no email is provided', async () => {
+    const { sut } = makeSut();
+    const httpRequest = {
+      body: {
+        name: faker.name.findName(),
+        password: faker.internet.password(),
+        passwordConfirmation: faker.internet.password(),
+      },
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new MissingParamError('email'));
+  });
 });
