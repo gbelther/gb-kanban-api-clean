48026e879e776d6062ca9d49651753cf7f6d87d7
gbelther <belthergabriel@gmail.com>
2022-06-13 23:21:19 -0300

    test: ensure return throw if email is not valid

--- src/presentation/controllers/SignUpController.spec.ts
+++ src/presentation/controllers/SignUpController.spec.ts
@@ -1,15 +1,29 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { faker } from '@faker-js/faker';
 import { InvalidParamError, MissingParamError } from '../errors';
+import { EmailValidator } from '../protocols/EmailValidator';
 import { SignUpController } from './SignUpController';
+
+const makeEmailValidator = (): EmailValidator => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid(email: string): boolean {
+      return true;
+    }
+  }
+  return new EmailValidatorStub();
+};
 
 type MakeSutReturn = {
   sut: SignUpController;
+  emailValidatorStub: EmailValidator;
 };
 
 const makeSut = (): MakeSutReturn => {
-  const sut = new SignUpController();
+  const emailValidatorStub = makeEmailValidator();
+  const sut = new SignUpController(emailValidatorStub);
   return {
     sut,
+    emailValidatorStub,
   };
 };
 
@@ -87,5 +101,22 @@ describe('SignUpController', () => {
     expect(httpResponse.body).toEqual(
       new InvalidParamError('passwordConfirmation')
     );
+  });
+
+  it('should return 400 if email is invalid', async () => {
+    const { sut, emailValidatorStub } = makeSut();
+    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false);
+    const password = faker.internet.password();
+    const httpRequest = {
+      body: {
+        name: faker.name.findName(),
+        email: faker.name.findName(),
+        password,
+        passwordConfirmation: password,
+      },
+    };
+    const httpResponse = await sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new InvalidParamError('email'));
   });
 });
