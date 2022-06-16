55b210f866486d4b57ae36e3de943ff7ed691d8e
gbelther <belthergabriel@gmail.com>
2022-06-16 09:48:40 -0300

    feat: create bcrypt adapter class

---
+++ src/infra/cryptography/bcrypt-adapter.ts
@@ -0,0 +1,10 @@
+import bcrypt from 'bcrypt';
+import { Hasher } from '../../data/protocols/cryptography';
+
+export class BcryptAdapter implements Hasher {
+  constructor(private readonly salt: number) {}
+
+  async hash(plaintext: string): Promise<string> {
+    return bcrypt.hash(plaintext, this.salt);
+  }
+}
